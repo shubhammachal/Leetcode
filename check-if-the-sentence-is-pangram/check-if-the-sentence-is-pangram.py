fc2fbe7eeef6d55a1b33d8ce35b277e102ccf422
Shubham Machal <shubham95m@gmail.com>
2024-12-31 17:18:23 -0800

    Time: 0 ms, Memory: 17.7 MB - LeetHub

--- check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
+++ check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -1,8 +1,3 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        if len(sentence) < 26:
-            return False
-        alphabet_set = set('abcdefghijklmnopqrstuvwxyz')
-        sen_set = set(sentence)
-        return alphabet_set.issubset(sen_set)
-            
+        return len(set(sentence)) == 26

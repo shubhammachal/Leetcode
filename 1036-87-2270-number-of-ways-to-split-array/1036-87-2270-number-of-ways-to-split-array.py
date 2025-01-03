a755256e7f9158e1c666124cad997335c63c080d
Shubham Machal <shubham95m@gmail.com>
2025-01-03 10:03:18 -0800

    Time: 55 ms, Memory: 32.3 MB - LeetHub

--- 1036-87-2270-number-of-ways-to-split-array/1036-87-2270-number-of-ways-to-split-array.py
+++ 1036-87-2270-number-of-ways-to-split-array/1036-87-2270-number-of-ways-to-split-array.py
@@ -1,6 +1,6 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
-        left_sum = right_sum = 0
+        left_sum = 0
         count = 0
         right_sum = sum(nums)
         for i in range(len(nums) - 1):

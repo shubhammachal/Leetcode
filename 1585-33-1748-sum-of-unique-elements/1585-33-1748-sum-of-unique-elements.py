73eb860916c06caad53e705737fdaad60dba8b11
Shubham Machal <shubham95m@gmail.com>
2025-01-03 10:57:03 -0800

    Time: 0 ms, Memory: 17.7 MB - LeetHub

--- 1585-33-1748-sum-of-unique-elements/1585-33-1748-sum-of-unique-elements.py
+++ 1585-33-1748-sum-of-unique-elements/1585-33-1748-sum-of-unique-elements.py
@@ -1,10 +1,5 @@
+from collections import Counter
 class Solution:
     def sumOfUnique(self, nums: List[int]) -> int:
-        hash_map = {}
-        sums = 0
-        for num in nums:
-            hash_map[num] = hash_map.get(num, 0 ) + 1
-        for num, value in hash_map.items():
-            if value < 2:
-                sums += num
-        return sums
+        counts = Counter(nums)
+        return sum(num for num, count in counts.items() if count == 1)

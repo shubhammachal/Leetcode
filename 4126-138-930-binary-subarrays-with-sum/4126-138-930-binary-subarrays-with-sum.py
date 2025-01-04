7cb8bd0cb2826dad4e2b9131abcd867c18c793ad
Shubham Machal <shubham95m@gmail.com>
2025-01-04 14:06:57 -0800

    Time: 31 ms, Memory: 21.4 MB - LeetHub

--- 4126-138-930-binary-subarrays-with-sum/4126-138-930-binary-subarrays-with-sum.py
+++ 4126-138-930-binary-subarrays-with-sum/4126-138-930-binary-subarrays-with-sum.py
@@ -1,13 +1,19 @@
 from collections import defaultdict
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        counts = defaultdict(int)
+        
+        # keep track of how often certain sum occur.
+        
+        counts = defaultdict(int) 
         counts[0] = 1
         
         ans = curr = 0
         for num in nums:
             curr += num
-            if curr - goal in counts:
-                ans += counts[curr-goal]
+            
+            #If curr - goal exists in counts, it means there are subarrays ending at the current position where the sum of the subarray equals goal.
+            
+            if curr - goal in counts: 
+                ans += counts[curr-goal] #counts[curr - goal] tells us how many times a prefix sum of curr - goal has been seen.
             counts[curr] += 1
         return ans

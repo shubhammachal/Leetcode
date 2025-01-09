7cd7c01266a7357304a91e6a61e29ca3d8cc105f
Shubham Machal <shubham95m@gmail.com>
2025-01-09 00:31:45 -0800

    Time: 0 ms, Memory: 18 MB - LeetHub

--- 0264-15-1427-perform-string-shifts/0264-15-1427-perform-string-shifts.py
+++ 0264-15-1427-perform-string-shifts/0264-15-1427-perform-string-shifts.py
@@ -1,17 +1,18 @@
 class Solution:
     def stringShift(self, s: str, shift: List[List[int]]) -> str:
-        #brute force
-        s = list(s)
         n = len(s)
+        total_shift = 0
+
+        # Calculate net shift
         for direction, amount in shift:
-            if direction == 0:
-                amount %= n
-                for _ in range(amount):
-                    first = s.pop(0)
-                    s.append(first)
-            elif direction == 1:
-                amount %= n
-                for  _ in range(amount):
-                    last = s.pop()
-                    s.insert(0,last)
-        return "".join(s)
+            if direction == 0:  # Left shift
+                total_shift -= amount
+            else:  # Right shift
+                total_shift += amount
+
+        # Reduce total shift within bounds of string length
+        total_shift %= n
+
+        # Perform the net shift
+        s = s[-total_shift:] + s[:-total_shift]
+        return s

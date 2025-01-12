f01ca15b8c5b5fc26a3aefe2a04144bdad301175
Shubham Machal <shubham95m@gmail.com>
2025-01-12 11:01:59 -0800

    Time: 52 ms, Memory: 31.1 MB - LeetHub

--- 1259-147-1762-buildings-with-an-ocean-view/1259-147-1762-buildings-with-an-ocean-view.py
+++ 1259-147-1762-buildings-with-an-ocean-view/1259-147-1762-buildings-with-an-ocean-view.py
@@ -1,6 +1,9 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
-        #monotonic stack
+        # constraint is iteration from left to right
+        # decreasing monotonic stack: if the current height is greater than or equal to the top of the stack
+        # pop from the stack and add current height to maintain the decreasing order
+        # add the current index to the stack
         stack = []
         for i in range(len(heights)):
             while stack and heights[i] >= heights[stack[-1]]:

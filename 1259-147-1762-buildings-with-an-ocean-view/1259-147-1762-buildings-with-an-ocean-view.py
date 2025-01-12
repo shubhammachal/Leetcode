b048c7c6b153cbc15dab891c3c456297a96176ec
Shubham Machal <shubham95m@gmail.com>
2025-01-12 10:58:07 -0800

    Time: 48 ms, Memory: 31.2 MB - LeetHub

--- 1259-147-1762-buildings-with-an-ocean-view/1259-147-1762-buildings-with-an-ocean-view.py
+++ 1259-147-1762-buildings-with-an-ocean-view/1259-147-1762-buildings-with-an-ocean-view.py
@@ -1,15 +1,11 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
-        res = []
-        n = len(heights)
-        max_so_far = heights[-1]
-        #rightmost index will always have ocean view
-        for i in range(n-1, -1, -1):
-            if heights[i] > max_so_far:
-                max_so_far = heights[i]
-                res.append(i)
-        res.append(n-1)
-        return sorted(res)
-                
+        #monotonic stack
+        stack = []
+        for i in range(len(heights)):
+            while stack and heights[i] >= heights[stack[-1]]:
+                stack.pop()
+            stack.append(i)
+        return stack
 
             

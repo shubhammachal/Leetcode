5bd334ebf7f4634cdf14266eb98730aba6f9e86b
Shubham Machal <shubham95m@gmail.com>
2025-01-13 10:54:40 -0800

    Time: 15 ms, Memory: 31 MB - LeetHub

--- 0189-25-2340-minimum-adjacent-swaps-to-make-a-valid-array/0189-25-2340-minimum-adjacent-swaps-to-make-a-valid-array.py
+++ 0189-25-2340-minimum-adjacent-swaps-to-make-a-valid-array/0189-25-2340-minimum-adjacent-swaps-to-make-a-valid-array.py
@@ -1,28 +1,14 @@
 class Solution:
     def minimumSwaps(self, nums: List[int]) -> int:
-        #intuition: find index of min from the start ans swap to bring min to front
-        # then find index of max to from the right ans swap 
-        #count total swap
+        min_idx = nums.index(min(nums))
         n = len(nums)
-        #edge case
-        if n == 1:
-            return 0
-        count = 0
-        s_index = nums.index(min(nums))
-        while nums[0] != nums[s_index]:
-            nums[s_index], nums[s_index - 1] = nums[s_index - 1], nums[s_index]
-            count += 1
-            s_index -= 1
-        #index of max from right
-        m_index = n - 1
-        for i in range(n - 1, -1, -1):
-            if nums[i] > nums[m_index]:
-                m_index = i
-        #swap to bring max to the last
 
-        while nums[n-1] != nums[m_index]:
-            nums[m_index], nums[m_index + 1] = nums[m_index + 1], nums[m_index]
-            count += 1
-            m_index += 1
+        max_idx = n-1
+        for i in range(n-2,-1,-1):
+            if nums[i] > nums[max_idx]:
+                max_idx = i
+        count = ((n - 1) - max_idx) + min_idx
+        #overlap of 1 swap if min_idx is farther away from max_idx
+        if min_idx > max_idx: 
+            count -= 1
         return count
-        

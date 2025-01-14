0204c75f7980feb66a7cd1936fd35484f5e38861
Shubham Machal <shubham95m@gmail.com>
2025-01-14 15:24:23 -0800

    Time: 3 ms, Memory: 17.7 MB - LeetHub

--- make-the-string-great/make-the-string-great.py
+++ make-the-string-great/make-the-string-great.py
@@ -1,8 +1,5 @@
 class Solution:
     def makeGood(self, s: str) -> str:
-        if len(s) == 1:
-            return s
-            
         stack = []
         for char in s:
             if stack and abs(ord(char) - ord(stack[-1])) == 32:

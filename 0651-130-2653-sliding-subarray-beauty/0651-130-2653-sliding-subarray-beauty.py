3776e052bb5aa1725ca157fd63a7c8eb16a674cd
Shubham Machal <shubham95m@gmail.com>
2025-01-21 11:43:26 -0800

    Time: 973 ms, Memory: 28 MB - LeetHub

--- 0651-130-2653-sliding-subarray-beauty/0651-130-2653-sliding-subarray-beauty.py
+++ 0651-130-2653-sliding-subarray-beauty/0651-130-2653-sliding-subarray-beauty.py
@@ -6,13 +6,19 @@ class Solution:
         n = len(nums)
         result = []
         negatives = SortedList()
+        #only add negatives to negatives list, it will store negative numbers in sorted order
         for i in range(n):
             if nums[i] < 0:
                 negatives.add(nums[i])
+            #check for window size: if it is greater than k, remove the leftmost
             if i >= k and nums[i - k ] < 0:
                 negatives.remove(nums[i-k])
+            
+            #if window size == k - 1, start appending to result
+            #if k = 3, first full window will be at index 2(3-1)
             if i >= k - 1:
                 if len(negatives) >= x:
+                    #since negatives is a sorted list, x-1 will be the xth smallest negative element
                     result.append(negatives[x - 1])
                 else:
                     result.append(0)

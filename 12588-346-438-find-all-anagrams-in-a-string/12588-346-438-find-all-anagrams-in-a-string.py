f527917bbf7d6e3d64252a9004b6241509961e2b
Shubham Machal <shubham95m@gmail.com>
2025-01-21 14:09:21 -0800

    Time: 229 ms, Memory: 18.4 MB - LeetHub

--- 12588-346-438-find-all-anagrams-in-a-string/12588-346-438-find-all-anagrams-in-a-string.py
+++ 12588-346-438-find-all-anagrams-in-a-string/12588-346-438-find-all-anagrams-in-a-string.py
@@ -3,19 +3,22 @@ class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         window_size = len(p)
         p_count = Counter(p)
-        s_count = Counter(s[:window_size - 1])
+        s_count = Counter()
         res = []
         left = 0
-        for right in range(window_size - 1, len(s)):
-            s_count[s[right]] += 1
-
-            if s_count == p_count:
-                res.append(left)
+        for right in range(len(s)):
+            s_count[s[right]] += 1  # Add the current character to the window
             
-            s_count[s[left]] -= 1
-            if s_count[s[left]] == 0:
-                del s_count[s[left]] 
-            left += 1
-        return res
+            # When window size exceeds required size, shrink it from the left
+            if right - left + 1 > window_size:
+                s_count[s[left]] -= 1
+                if s_count[s[left]] == 0:
+                    del s_count[s[left]]  # Remove char from dictionary to avoid clutter
+                left += 1  # Move the left pointer
 
+            # When the window size matches, compare frequency counts
+            if right - left + 1 == window_size:
+                if s_count == p_count:
+                    res.append(left)
     
+        return res

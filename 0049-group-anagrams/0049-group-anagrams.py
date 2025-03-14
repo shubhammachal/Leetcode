1f2905b9f52101acecfa29020af7aaf3bd0933dc
Shubham Machal <shubham95m@gmail.com>
2025-03-13 21:30:13 -0700

    Time: 15 ms (51.38%), Space: 21.1 MB (51.04%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,12 @@
+from collections import defaultdict 
+#defualt dictionary used to avoid key errors. a default value is automatically returned or created for that key, eliminating the need to manually check for key existence.
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = []
+        anagram_dict = defaultdict(list)
+        for word in strs:
+            sorted_word = str(sorted(word))
+            anagram_dict[sorted_word].append(word)
+        for value in anagram_dict.values():
+            res.append(value)
+        return res

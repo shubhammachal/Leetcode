f5c9e1c6af4efcce8418ad964a6c6235222d3410
Shubham Machal <shubham95m@gmail.com>
2025-04-16 10:24:52 -0700

    Time: 171 ms (32.04%), Space: 34.3 MB (82.85%) - LeetHub

--- 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.py
+++ 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.py
@@ -2,7 +2,7 @@ from collections import Counter
 class Solution:
     def countGood(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        counter = Counter()
+        counter = Counter() #keep track of current count of number in the window
         pairs = 0
         ans = 0
         l = 0
@@ -11,7 +11,8 @@ class Solution:
             counter[num] += 1
 
             while pairs >= k:
-                ans += n-r
+                ans += n-r #if we found good subarray at r, the subarray starts
+                #at l and ends at r or r+1 or after that is also a good subarray
                 counter[nums[l]] -= 1
                 pairs -= counter[nums[l]]
                 l+= 1

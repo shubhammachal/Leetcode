85c17e00e726e3391cab71f3d4e989239407d373
Shubham Machal <shubham95m@gmail.com>
2025-04-22 10:58:13 -0700

    Time: 0 ms (100%), Space: 17.7 MB (70.1%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,48 +1,21 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        def isValidRows(board):
-            for row in range(9):
-                seen = set()
-                for col in range(9):
-                    if board[row][col] == "." or board[row][col] == '0':
-                        continue
-                    if board[row][col] in seen:
-                        return False
-                    seen.add(board[row][col])
-            return True 
-        
-        def isValidCols(board):
+        #can map each grid as 0 to 8 and then check
+
+        rows = [set () for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        grids = [set() for _ in range(9)]
+
+        for row in range(9):
             for col in range(9):
-                seen = set()
-                for row in range(9):
-                    if board[row][col] == "." or board[row][col] == '0':
-                        continue
-                    if board[row][col] in seen:
-                        return False
-                    seen.add(board[row][col])
-            return True
-        
-        def isValidGrid(board):
-            for box_row in range(3):
-                for box_col in range(3):
-                    seen = set()
-                    
-                    start_row = box_row * 3
-                    start_col = box_col * 3
-                    
-                    
-                    for row_offset in range(3):
-                        for col_offset in range(3):
-                            row = start_row + row_offset
-                            col = start_col + col_offset
-                            
-                            if board[row][col] == '.' or board[row][col] == '0':  
-                                continue
-                            
-                            if board[row][col] in seen:
-                                return False
-                            seen.add(board[row][col])
-            return True  
-        
-   
-        return isValidRows(board) and isValidCols(board) and isValidGrid(board)
+                val = board[row][col]
+                if val == '0' or val == '.':
+                    continue
+                grid_index = (row // 3) * 3 + (col // 3)
+
+                if val in rows[row] or val in cols[col] or val in grids[grid_index]:
+                    return False
+                rows[row].add(val)
+                cols[col].add(val)
+                grids[grid_index].add(val)
+        return True

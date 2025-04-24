6c7e80d0026ee7ebccc8cbdb73956553ee3d818a
Shubham Machal <shubham95m@gmail.com>
2025-04-23 22:39:44 -0700

    Time: 476 ms (24.3%), Space: 18.2 MB (30.24%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.py
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.py
@@ -2,10 +2,12 @@ class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
         number_of_distinct = len(set(nums))
         count = 0
-        for i in range(len(nums)):
+        n = len(nums)
+        for i in range(n):
             unique = set()
-            for j in range(i,len(nums)):
+            for j in range(i,n):
                 unique.add(nums[j])
                 if len(unique) == number_of_distinct:
-                    count += 1
+                    count += n - j
+                    break
         return count

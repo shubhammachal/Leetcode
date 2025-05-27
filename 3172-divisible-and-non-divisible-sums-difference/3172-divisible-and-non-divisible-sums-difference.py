b324187070658161f1c37e2dc76bfac32f197f60
Shubham Machal <shubham95m@gmail.com>
2025-05-27 11:50:31 -0700

    Time: 0 ms (100%), Space: 17.8 MB (67.05%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.py
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.py
@@ -5,8 +5,6 @@ class Solution:
         #edge cases
         if m > n:
             return total_sum
-        if n == 1:
-            return -1 if m == 1 else 1
         
         # multiples of m in the range[1,n] let's say k
         k = n // m

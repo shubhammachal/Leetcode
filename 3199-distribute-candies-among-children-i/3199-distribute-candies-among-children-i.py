1e940db5fe495c4cc5463dc0c2426434b8be186d
Shubham Machal <shubham95m@gmail.com>
2025-07-15 17:21:13 -0700

    Time: 27 ms (40.8%), Space: 17.9 MB (20.45%) - LeetHub

--- 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.py
+++ 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.py
@@ -1,9 +1,10 @@
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
-        ways = 0
-        for i in range(limit+1):
-            for j in range(limit+1):
-                for k in range(limit+1):
-                    if i + j + k == n:
-                        ways += 1
-        return ways
+        number_of_ways = 0
+        for a in range(limit+1):
+            for b in range(limit+1):
+                c = n - (a + b)
+                if 0 <= c <= limit and a + b + c == n:
+                    number_of_ways += 1
+                
+        return number_of_ways

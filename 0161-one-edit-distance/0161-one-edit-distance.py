6913bccb30bc0d5bf10d317270d3d272f0bacdd4
Shubham Machal <shubham95m@gmail.com>
2025-08-31 20:18:48 -0700

    Time: 4 ms (12.7%), Space: 17.6 MB (94.05%) - LeetHub

--- 0161-one-edit-distance/0161-one-edit-distance.py
+++ 0161-one-edit-distance/0161-one-edit-distance.py
@@ -5,18 +5,21 @@ class Solution:
         if abs(n - m) > 1:
             return False
         i = 0 
-
+        #will find the first index where the characters are different
         while i < n and i < m and s[i] == t[i]:
             i += 1
- 
+        #if we reach the end of both the strings and can't find such character,
+        #return False
         if i == n and i == m:
             return False
-
+        #if lengths are not equal and diff char found is at index i
+        #we will compare rest of the characters are same or not
         if n < m:
             return s[i:] == t[i+1:]
         elif n > m:
             return s[i+1:] == t[i:]
-        
+        #if lengths are equal and diff char is at index i
+        #check if rest of the string is same
         else:
             return s[i+1:] == t[i+1:]
 

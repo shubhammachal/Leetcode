e9f510acc300530648c5384b23ff006cad8ccdf0
Shubham Machal <shubham95m@gmail.com>
2025-09-10 12:33:17 -0700

    Time: 24 ms (31.22%), Space: 17.9 MB (39.23%) - LeetHub

--- 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.py
+++ 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.py
@@ -3,7 +3,7 @@ class Solution:
         count = 0
         for i in range(limit + 1):
             for j in range(limit + 1):
-                for k in range(limit + 1):
-                    if i + j + k == n:
-                        count += 1
+                k = n - i - j
+                if 0 <= k <= limit:
+                    count += 1
         return count
